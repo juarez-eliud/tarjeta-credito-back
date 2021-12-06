ed4bcd63bedfaf0c08d047a95022f18a348a7403
Eliud Juárez <juarez_s3@hotmail.com>
2021-12-05 18:39:01 -0600

    Initial commit

---
+++ FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FBTarjeta.Models;
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext : DbContext
+    {
+
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }      
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
